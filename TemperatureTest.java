ab5d4a48aba7291c127b27ce53b3e1e393fe6e3c
alonsomedina <alonso.medina@mail.mcgill.ca>
2013-10-08 12:53:07 -0400

    Added test case
    Added nominal test case for getValue().

--- TemperatureTest.java
+++ TemperatureTest.java
@@ -12,13 +12,18 @@ public class TemperatureTest {
   // Add multiple tests to check all functions of
   // {@Code Temperature} class.
 
-  // @Test
-  // public void .... (){
-  //    ...
-  // }
+  @Test
+  public void testGetValue(){
+    //nominal test case
+     System.out.println("Testing if getValue gets the correct value... ");
+     Temperature nominalTestCase = new Temperature (15, Temperature.CELSIUS);
+     assertTrue(nominalTestCase.getValue() == 15);
+     Temperature nominalTestCaseConverted = new Temperature(nominalTestCase);
+     assertTrue(nominalTestCaseConverted.getValue() == 15);
+  }
 
-  // @Test
-  // public void .... (){
-  //    ...
-  // }
+  //@Test
+  //public void .... (){
+    
+  //}
 }
